a439ff3d10298c37ddbcf4a40141dc6a702f246a
tankh99 <khanghou99@gmail.com>
2024-06-17 09:09:09 +0800

    feat: add quill editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import NoteList from "@/components/NoteList";
+import Editor from "@/components/editor";
+import NoteList from "@/components/note-list";
 import Image from "next/image";
 
 export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Editor/>
       <NoteList/>
+      
     </main>
   );
 }
